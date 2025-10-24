d85058505f1c12ddca12d585329b8360bd332288
ElVenJi <benjaminalvaradoguerra@gmail.com>
2025-10-24 17:51:48 -0300

    tarea3

---
+++ flask_app/Tarea3/database/create_user.sql
@@ -0,0 +1,6 @@
+CREATE USER 'cc5002'@'localhost' IDENTIFIED BY 'programacionweb';
+
+GRANT ALL PRIVILEGES ON tarea2.* TO 'cc5002'@'localhost';
+
+-- Eliminar usuario de ser necesario
+DROP USER 'cc5002'@'localhost';
